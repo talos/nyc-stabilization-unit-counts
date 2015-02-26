91f60ddd8e4ec075cc9c7c7dea72329b90a9564e
Ziggy <ziggy@elephant-bird.net>
2015-02-26 15:16:24 -0500

    work of recursive_parse

--- recursive_parse.js
+++ recursive_parse.js
@@ -1,10 +1,10 @@
 function parse_pdf(text) {
 
   var pdf = text.split(' ');
-  var property = {}
+  var taxDoc = {}
   parsing(pdf);
-  // additional property formating here
-  return property; 
+  // additional taxDoc formating here
+  return taxDoc; 
   
   function parsing(arr){
     // base case
@@ -12,13 +12,24 @@ function parse_pdf(text) {
       return;
     }
 
-    var word = pdf[0];
+    var word = arr[0];
 
-    if (/Activty/.test(word)) {
-      // do something with it
-      property.activityThrough = pdf[1] + pdf[2] + pdf[3];
-      parser(arr.slice(1));
-    } else if ( another-case) {
+    if (/Activty/.test(word) && /through\s*/.test(arr[1])) {
+      taxDoc.activityThrough = arr[2] + arr[3] + arr[4];
+      parser(arr.slice(4));
+    } else if (/Owner/.test(word) && /name:/.test(arr[1])) {
+
+       
+            if (/(\d{6,})|Mailing|Quarterly/.test(arr[2]) {
+        // end of word
+        taxDoc.ownerName = ownerName + arr[2]
+
+      }
+      if(arr[2])
+
+      if (arr[2])
+      // 
+      Owner name: [large number, Mailing, Quarterly, 
       // do something with it
     } else {
       // don't need this word
