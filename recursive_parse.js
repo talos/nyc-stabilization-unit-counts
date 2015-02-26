8a257ba7f7f96e88b85e60dc5e87200771b06a1a
aepyornis <ziggy@elephant-bird.net>
2015-02-26 16:44:59 -0500

    activityThrough, ownerName, propertyAddress, bbl, mailingAddress done

--- recursive_parse.js
+++ recursive_parse.js
@@ -1,40 +1,88 @@
-function parse_pdf(text) {
+var textract = require('textract');
+var _ = require('underscore');
 
-  var pdf = text.split(' ');
-  var taxDoc = {}
-  parsing(pdf);
+main('samplePDFs/sample1.pdf');
+main('samplePDFs/sample2.pdf');
+main('samplePDFs/sample3.pdf');
+main('samplePDFs/sample4.pdf');
+main('samplePDFs/sample5.pdf');
+main('samplePDFs/sample6.pdf');
+main('samplePDFs/sample7.pdf');
+main('samplePDFs/sample8.pdf');
+main('samplePDFs/sample9.pdf');
+main('samplePDFs/sample10.pdf');
+
+function main(filepath) {
+  textract('application/pdf', filepath, function(err, text){
+
+    var taxDoc = parse_pdf(text.split(" "));
+    console.log(taxDoc);
+  })
+}
+
+
+
+function parse_pdf(arr) {
+
+  var taxDoc = {
+    activityThrough: null,
+    ownerName: '',
+    propertyAddress: '',
+    bbl: '',
+    mailingAddress: ''
+  }
+  parsing(arr);
   // additional taxDoc formating here
   return taxDoc; 
-  
+
   function parsing(arr){
     // base case
-    if (arr == []) {
-      return;
-    }
+    if (_.isEmpty(arr)) { return; }
 
     var word = arr[0];
 
-    if (/Activty/.test(word) && /through\s*/.test(arr[1])) {
-      taxDoc.activityThrough = arr[2] + arr[3] + arr[4];
-      parser(arr.slice(4));
+    if (/Activity/.test(word) && /through\s*/.test(arr[1])) {
+      taxDoc.activityThrough = arr[2] + " " + arr[3] + " " + arr[4];
+      parsing(arr.slice(4));
     } else if (/Owner/.test(word) && /name:/.test(arr[1])) {
-
-       
-            if (/(\d{6,})|Mailing|Quarterly/.test(arr[2]) {
-        // end of word
-        taxDoc.ownerName = ownerName + arr[2]
-
+      // look for for end of owner name
+      for (var i = 2; i < arr.length; i++) {
+        // if over - recurse
+        if(/(\d{6,})|Mailing|Quarterly/.test(arr[i])) {
+          parsing(arr.slice(i))
+          return;
+        } else {
+          taxDoc.ownerName = taxDoc.ownerName + arr[i] + " ";
+        }
       }
-      if(arr[2])
+    } else if (arr[0] === 'Property' && arr[1] === 'address:') {
+      for (var i = 2; i < arr.length; i++) {
+        if (/Borough,|Property/.test(arr[i])){
+          parsing(arr.slice(i));
+          return;
+        } else {
+          taxDoc.propertyAddress = taxDoc.propertyAddress + arr[i] + " ";
+        }
+      }
+    } else if (arr[0] === 'Borough,' && arr[1] === 'block' && arr[2] === '&' && arr[3] === 'lot:') {
+      taxDoc.bbl = arr[4] + arr[5] + arr[6] + arr[7];
+      parsing(arr.slice(7));
+    } else if (arr[0] === 'Mailing' && arr[1] === 'address:') {
 
-      if (arr[2])
-      // 
-      Owner name: [large number, Mailing, Quarterly, 
-      // do something with it
-    } else {
-      // don't need this word
-      parser(arr.slice(1))
+      // case if property address shows up after mailing address
+
+      for (var i =2; i < arr.length; i++){
+        if (/Statement|Outstanding|\$0.00/.test(arr[i])){
+          parsing(arr.slice(i));
+          return;
+        } else {
+          taxDoc.mailingAddress = taxDoc.mailingAddress + arr[i] + " ";
+        }
+      }
+
     }
+    // don't need this word
+    else { parsing(arr.slice(1)) }
 
   }
 
