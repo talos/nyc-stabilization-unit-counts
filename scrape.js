cdc460a715884cf68eb31355f0577e18a864f45b
aepyornis <ziggy@elephant-bird.net>
2015-02-26 18:56:03 -0500

    all fields pass except bbl

--- scrape.js
+++ scrape.js
@@ -1,49 +1,157 @@
 var textract = require('textract');
 var _ = require('underscore');
 
-main('samplePDFs/sample6.pdf', function(pdf){console.log(pdf)});
 
 
-function main(filepath, callback) {
+var parse = function (filepath, callback) {
   textract('application/pdf', filepath, function(err, text){
-    // var pdf = text.split(' ');
-    // var bill = {}
-    callback(text);
+    var taxDoc = parse_pdf(text.split(" "));
+    callback(taxDoc);
   })
 }
 
-// function activity_through(pdf) {
-//   for (i = 0; i < pdf.length; i ++) {
-//     if (/Activity/.test(text) && /through/.test(pdf[i+1])) {
-//       return pdf[i+2] + pdf[i+3] + pdf[i+3]
+
+
+function parse_pdf(arr) {
+
+  var taxDoc = {
+    activityThrough: null,
+    ownerName: '',
+    propertyAddress: '',
+    bbl: '',
+    mailingAddress: '',
+    rentStabilized: null,
+    units: null,
+    annualPropertyTax: null,
+    abatements: [],
+    billableAssessedValue: null,
+    taxRate: null
+  }
+  parsing(arr);
+  // additional taxDoc formating here
+  annualPropertyTax(arr);
+  return cleanUp(taxDoc); 
+
+  function parsing(arr){
+    // base case
+    if (_.isEmpty(arr)) { return; }
+
+    var word = arr[0];
+
+    if (/Activity/.test(word) && /through\s*/.test(arr[1])) {
+      taxDoc.activityThrough = arr[2] + " " + arr[3] + " " + arr[4];
+      parsing(arr.slice(4));
+    } else if (/Owner/.test(word) && /name:/.test(arr[1])) {
+      // look for for end of owner name
+      ownerName(arr);
+    } else if (arr[0] === 'Property' && arr[1] === 'address:') {
+      propertyAddress(arr);
+    } else if (arr[0] === 'Borough,' && arr[1] === 'block' && arr[2] === '&' && arr[3] === 'lot:') {
+      taxDoc.bbl = arr[4] + arr[5] + arr[6] + arr[7];
+      parsing(arr.slice(7));
+    } else if (arr[0] === 'Mailing' && arr[1] === 'address:') {
+      // case if property address shows up after mailing address
+      mailingAddress(arr);
+    } else if (arr[0] === 'Housing-Rent' && arr[1] === 'Stabilization') {
+      stabilization(arr);
+    } else if (/J-51|Mitchell|421a/g.test(arr[0])) {
+      taxDoc.abatements.push(arr[0]);
+      parsing(arr.slice(1));
+    } else if (/^\$\d+,?\d+/.test(arr[0]) && arr[1] === 'X' && /\d+\.\d+%$/.test(arr[2])) {
+      taxDoc.billableAssessedValue = arr[0];
+      taxDoc.taxRate = arr[2];
+      parsing(arr.slice(3));
+    } else { 
+       // don't need this word
+      parsing(arr.slice(1)) }
+
+  }
+
+  function ownerName(arr) {
+    for (var i = 2; i < arr.length; i++) {
+        // if over - recurse
+        if(/(\d{6,})|Mailing|Quarterly/.test(arr[i])) {
+          parsing(arr.slice(i))
+          return;
+        } else {
+          taxDoc.ownerName = taxDoc.ownerName + arr[i] + " ";
+        }
+    }
+  }
+
+  function propertyAddress(arr) {
+    for (var i = 2; i < arr.length; i++) {
+        if (/Borough,|Property/.test(arr[i])){
+          parsing(arr.slice(i));
+          return;
+        } else {
+          taxDoc.propertyAddress = taxDoc.propertyAddress + arr[i] + " ";
+      }
+    }
+  }
+
+  function mailingAddress(arr) {
+    for (var i =2; i < arr.length; i++){
+      if (/Statement|Outstanding|\$0.00/.test(arr[i])){
+        parsing(arr.slice(i));
+        return;
+      } else {
+        taxDoc.mailingAddress = taxDoc.mailingAddress + arr[i] + " ";
+      }
+    }
+  }
+
+  function stabilization(arr) {
+    taxDoc.rentStabilized = true;
+    if (/\d{1,4}/.test(arr[2])) {
+      taxDoc.units = arr[2];
+      parsing(arr.slice(2))
+    } else if (/\d{1,4}/.test(arr[7])) {
+      taxDoc.units = arr[7];
+      parsing(arr.slice(7));
+    } else {
+      for (var i = 7; i < arr.length; i++) {
+        if (arr[i] === 'remaining') {
+          taxDoc.units = arr[i+1];
+          parsing(arr.slice(i));
+          return;
+        }
+      }
+    }
+  }
+
+  function abatements(arr) {
+
+  }
+
+  function annualPropertyTax(arr) {
+    var tax_index = _.findLastIndex(arr, function(val){
+      return (/\$\d+,?\d*\*\*/.test(val))
+    })
+    taxDoc.annualPropertyTax = arr[tax_index];
+  }
+
+  // input: taxDoc
+  // output: cleaned up taxDoc
+  function cleanUp(taxDoc) {
+    var clean = taxDoc;
+
+    if (!clean.rentStabilized) {
+      clean.rentStabilized = false;
+      clean.units = 0;
+    }
+
+    clean.propertyAddress = clean.propertyAddress.trim();
+    clean.mailingAddress = clean.mailingAddress.trim();
+    clean.ownerName = clean.ownerName.trim();
+    clean.annualPropertyTax = clean.annualPropertyTax.replace("**", '');
+    clean.abatements = _.uniq(clean.abatements);
     
-//   }
-// }
+    return clean;
 
+  }
 
-// function activity_through(pd) {
-
-//   _.chain(pdf).findIndex(function(val){
-//     return /Activity/.test(val);
-//   }).tap(function(index){
-
-//     if (/through/.test(pdf[index+1])) {
-//       return pdf[i+2] + pdf[i+3] + pdf[i+3];
-//     } else {
-//       activity_through(pdf.slice(index+1))
-//     }
-
-//   }).value();
-// }
-
-module.exports = {
-
-  main: main
+// end of parse_pdf  
 }
 
-  // _.each(pdf, function(text, i, l){
-  //   if (/Activity/.test(text) && /through/.test(pdf[i+1])) {
-  //     activity = pdf[i+2] + pdf[i+3] + pdf[i+3]
-  //   }
-  // })
-  // return activity;
+module.exports = parse; 
--- scrape.spec.js
+++ scrape.spec.js
@@ -1,27 +1,28 @@
 var should = require('should');
-var pdf = require('./scrape')
+var parse = require('./scrape')
 
 
 var result = {
   activityThrough: 'August 22, 2014',
   ownerName: 'PARKER ENG LLC',
-  propertyAdd: '2425 STEINWAY ST.',
+  propertyAddress: '2425 STEINWAY ST.',
   bbl: '4006860007',
-  mailingAddress: 'PARKER ENG LLC\n18422 GRAND CENTRAL PKWY\nJAMAICA, NY 11432-5801',
+  mailingAddress: 'PARKER ENG LLC 18422 GRAND CENTRAL PKWY. JAMAICA, NY 11432-5801',
   rentStabilized: true,
   units: '12',
   annualPropertyTax: '$38,804',
   abatements: [],
-  taxRate: '13.1450%'
+  taxRate: '13.1450%',
+  billableAssessedValue: '$295,200'
 }
 
 
-describe('sample 3', function(){
+describe('sample 2', function(){
 // this.timeout(15000);
 var sample3;
   before(function(done){
 
-    pdf.main('samplePDFs/sample3.pdf', function(pdf){
+    parse('samplePDFs/sample2.pdf', function(pdf){
       sample3 = pdf;
       done();
     })
