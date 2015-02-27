e4efc83ae68656de135cade9b4d2860e3861fe0f
Dan Kass <romeboards@Dans-MacBook-Pro-4.local>
2015-02-27 10:58:32 -0500

    adding sample data

--- property-value/outputs.sh
+++ property-value/outputs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-node scrape.js data/sample_01152010.pdf > output/2010.out
-node scrape.js data/sample_01152011.pdf > output/2011.out
-node scrape.js data/sample_01152012.pdf > output/2012.out
-node scrape.js data/sample_01152013.pdf > output/2013.out
-node scrape.js data/sample_01152014.pdf > output/2014.out
-node scrape.js data/sample_01152015.pdf > output/2015.out
+node scrape.js samples/sample_01152010.pdf > output/2010.out
+node scrape.js samples/sample_01152011.pdf > output/2011.out
+node scrape.js samples/sample_01152012.pdf > output/2012.out
+node scrape.js samples/sample_01152013.pdf > output/2013.out
+node scrape.js samples/sample_01152014.pdf > output/2014.out
+node scrape.js samples/sample_01152015.pdf > output/2015.out
--- property-value/test.sh
+++ property-value/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-node scrape.js data/sample_01152010.pdf
-node scrape.js data/sample_01152011.pdf
-node scrape.js data/sample_01152012.pdf
-node scrape.js data/sample_01152013.pdf
-node scrape.js data/sample_01152014.pdf
-node scrape.js data/sample_01152015.pdf
+node scrape.js samples/sample_01152010.pdf
+node scrape.js samples/sample_01152011.pdf
+node scrape.js samples/sample_01152012.pdf
+node scrape.js samples/sample_01152013.pdf
+node scrape.js samples/sample_01152014.pdf
+node scrape.js samples/sample_01152015.pdf
