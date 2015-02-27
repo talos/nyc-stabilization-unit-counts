1be4a27bf55ca8eaf0917442730f3e36521656ca
aepyornis <ziggy@elephant-bird.net>
2015-02-27 12:25:10 -0500

    child_process

--- child_process.js
+++ child_process.js
@@ -1,10 +1,30 @@
 var exec = require('child_process').exec;
+var parse = require('./scrape');
+var fs = require('fs');
 
-exec('python download.py 1 1576 5', function(error, stdout, stderr){
+console.log('computer is downloading tax documents');
+exec('python download.py 4 1238 61', function(error, stdout, stderr){
   if (error !== null) {
     console.log('exec error: ' + error)
+  } else {
+    console.log('finished downloading')
+    console.log('starting to scrape...')
+    scrape_pdfs();
   }
-
-  console.log('finished downloading')
-
+  
 })
+
+
+function scrape_pdfs() {
+
+  var filePaths = fs.readdirSync('data/4-01238-0061');
+  
+  filePaths.forEach(function(file){
+    var path = 'data/4-01238-0061/' + file;
+    parse(path, function(tax){ console.log(tax) });
+  })
+
+}
+
+
+
