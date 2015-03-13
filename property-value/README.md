82605ecea8081f393999599b792a8440db49b60c
John Krauss <irving.krauss@gmail.com>
2015-03-13 15:43:34 -0400

    Adding README and removing redundant test/outputs scripts

---
+++ property-value/README.md
@@ -0,0 +1,9 @@
+Scraper to extract text data from NoPV (Notice of Property Value) PDFs.
+
+### Installation
+
+    npm install
+
+### Use
+
+    node scrape.js /path/to/nopv.pdf > outputfile.txt
--- property-value/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-node scrape.js samples/sample_01152010.pdf
-node scrape.js samples/sample_01152011.pdf
-node scrape.js samples/sample_01152012.pdf
-node scrape.js samples/sample_01152013.pdf
-node scrape.js samples/sample_01152014.pdf
-node scrape.js samples/sample_01152015.pdf
