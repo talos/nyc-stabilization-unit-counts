7c665b71a1b54115e74e1ba884ed517bb2d54b33
John Krauss <irving.krauss@gmail.com>
2015-03-13 16:15:39 -0400

    code cleanup, read in multiple files, write errors to stderr and keep going, write as CSV

--- property-value/README.md
+++ property-value/README.md
@@ -6,4 +6,12 @@ Scraper to extract text data from NoPV (Notice of Property Value) PDFs.
 
 ### Use
 
-    node scrape.js /path/to/nopv.pdf > outputfile.txt
+Can be used with single file:
+
+    node scrape.js /path/to/nopv.pdf > outputfile.csv
+
+Can also be used with multiple files:
+
+    node scrape.js /path/to/input/*.pdf > outpufile.csv
+
+A CSV is written to stdout, with errors written to stderr.
--- property-value/scrape.js
+++ property-value/scrape.js
@@ -1,42 +1,56 @@
+/*jshint node: true*/
+/*globals require*/
+
 var textract = require('textract');
 var _ = require('underscore');
- 
-var pathToPdf = __dirname + "/" + process.argv[2];
- 
-textract('application/pdf', pathToPdf, {preserveLineBreaks:true}, function( error, text ) {
-  console.log(to_json(text.split('\n')));
-});
+var incomeTest1 =
+  /(\s*)Gross Income:(\s*)We estimated gross income at \$(.*)\./;
+var incomeTest2 = /Estimated Gross Income:(\s*)\$(.*)/;
+var expensesTest1 = /(\s*)Expenses:(\s*)We estimated expenses at \$(.*)\./;
+var expensesTest2 = /Estimated Expenses:(\s*)\$(.*)/;
+var headers = ['estimatedGrossIncome', 'estimatedExpenses'];
 
-function to_json(chunks) {
+function parse (chunks) {
 
-  var prop = {
-    estimatedGrossIncome: '',
-    estimatedExpenses: ''
-  };
+  //var docDate, docYear;
+  var income, expenses;
 
-  var doc_date, doc_year;  
-
-  _.each(chunks, function(element, i, list) {
-
-    if ( /(\s*)Gross Income:(\s*)We estimated gross income at \$(.*)\./.test(element)){
-        var grossIncome = /(\s*)Gross Income:(\s*)We estimated gross income at \$(.*)\./.exec(element)[3];
-        prop.estimatedGrossIncome = grossIncome;
+  _.each(chunks, function(element) {
+    if (incomeTest1.test(element)) {
+      income = incomeTest1.exec(element)[3];
+    } else if (incomeTest2.test(element)){
+      income = incomeTest2.exec(element)[2];
     }
-    else if ( /Estimated Gross Income:(\s*)\$(.*)/.test(element)){
-      var grossIncome = /Estimated Gross Income:(\s*)\$(.*)/.exec(element)[2];
-      prop.estimatedGrossIncome = grossIncome;
-    }   
 
-    if ( /(\s*)Expenses:(\s*)We estimated expenses at \$(.*)\./.test(element)){
-        var expenses = /(\s*)Expenses:(\s*)We estimated expenses at \$(.*)\./.exec(element)[3];
-        prop.estimatedExpenses = expenses;
-    } 
-    else if ( /Estimated Expenses:(\s*)\$(.*)/.test(element)){
-      var expenses = /Estimated Expenses:(\s*)\$(.*)/.exec(element)[2];
-      prop.estimatedExpenses = expenses;
-    }       
-
+    if (expensesTest1.test(element)){
+      expenses = expensesTest1.exec(element)[3];
+    }
+    else if (expensesTest2.test(element)){
+      expenses = expensesTest2.exec(element)[2];
+    }
   });
 
-  return JSON.stringify(prop);
+  return {
+    estimatedGrossIncome: income ? Number(income.replace(/,/g, '')) : '',
+    estimatedExpenses: expenses ? Number(expenses.replace(/,/g, '')) : ''
+  };
 }
+
+console.log(headers.join(','));
+
+_.each(process.argv, function (path, i) {
+  if (i < 2) {
+    return;
+  }
+  var absPath = __dirname + "/" + path;
+
+  textract('application/pdf', absPath, {
+    preserveLineBreaks:true
+  }, function( error, text ) {
+    if (error) {
+      console.error("Could not read '" + absPath + "': " + error);
+    } else {
+      console.log(_.values(_.pick(parse(text.split('\n')), headers)).join(','));
+    }
+  });
+});
