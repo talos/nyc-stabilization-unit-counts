1977d79f5ae21dfd4c3fa4bcd2081d185d635067
John Krauss <irving.krauss@gmail.com>
2015-03-19 10:35:40 -0400

    Adding README, multi-file run.js and csv output

---
+++ tax-bills/README.md
@@ -0,0 +1,30 @@
+Scraper to extract text data from Quarterly Property Tax Bills.
+
+### Installation
+
+You will need to have installed [xpdf][] on your system.
+
+  [xpdf]: http://www.foolabs.com/xpdf/download.html
+
+You will also need node and npm.  Then, in this directory:
+
+    npm install
+
+### Use as script
+
+Can be used with single file:
+
+    node run.js /path/to/nopv.pdf > outputfile.csv
+
+Can also be used with multiple files:
+
+    node run.js /path/to/input/*.pdf > outpufile.csv
+
+A CSV is written to stdout, with errors written to stderr.
+
+### Use as library
+
+You can also use the scraper directly in a node script.
+
+    scraper = require('./scrape.js')
+    scraper('path/to/file.pdf', callback(data));
--- tax-bills/run.js
+++ tax-bills/run.js
@@ -1,9 +1,43 @@
 /*jshint node: true*/
 
 var scraper = require('./scrape.js');
+var _ = require('underscore');
+
+var lock = false; // poor man's synchronization.  <3 node.
+var headers = [
+    "activityThrough",
+    "ownerName",
+    "propertyAddress",
+    "bbl",
+    "mailingAddress",
+    "rentStabilized",
+    "units",
+    "annualPropertyTax",
+    "abatements",
+    "billableAssessedValue",
+    "taxRate"
+];
 
 function callback(taxDoc) {
-  console.log(taxDoc);
+  console.log(_.values(_.pick(taxDoc, headers)).join(','));
+  lock = false;
 }
 
-scraper(process.argv[2], callback);
+console.log(headers.join(','));
+
+_.each(process.argv, function (path, i) {
+  if (i < 2) {
+    return;
+  }
+  var synchronize = setInterval(function () {
+    if (lock === true) {
+      return;
+    } else {
+      lock = true;
+    }
+
+    scraper(path, callback);
+    clearInterval(synchronize);
+  }, 10);
+});
+
