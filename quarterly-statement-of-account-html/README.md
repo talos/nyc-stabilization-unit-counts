e395ec3cb14133d53f5a22c3fcda00106d1925ae
John Krauss <irving.krauss@gmail.com>
2015-03-20 11:23:48 -0400

    first stab at quarterly statement of account scraper

---
+++ quarterly-statement-of-account-html/README.md
@@ -0,0 +1,20 @@
+Scraper to extract text data from Quarterly Statement of Account HTML
+
+### Installation
+
+You will need to make sure to have installed all of `requirements.txt` in the
+root directory.
+
+    pip install requirements.txt
+
+This is best done in a `virtualenv`.
+
+### Usage
+
+Output is written as a CSV to stdout.  Input can be any number of HTML files.
+
+    python scrape.py /path/to/statement.html > outputfile.csv
+
+Or
+
+    python scrape.py /path/to/statements/*.html > outputfile.csv
---
+++ quarterly-statement-of-account-html/scrape.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+import csv
+import os
+import re
+from dateutil import parser
+from bs4 import BeautifulSoup
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler(sys.stderr))
+
+HEADERS = [
+    "bbl",
+    "activityThrough",
+    "rentStabilized",
+    "units",
+    "ownerName",
+    "propertyAddress",
+    "mailingAddress",
+    "annualPropertyTax",
+    "abatements",
+    "billableAssessedValue",
+    "taxRate"
+]
+
+
+def property_address(soup):
+    el = soup.find(text=re.compile('Owner Name:'))
+    return [c for c in el.parent.parent.children][-1].strip()
+
+
+def owner_name(soup):
+    el = soup.find(text=re.compile('Owner Name:'))
+    return [c for c in el.parent.parent.children][-1].strip()
+
+
+def activity_through(soup):
+    el = soup.find(text=re.compile('Reflects Account Activity from'))
+    match = re.search(r'Statement through\s+([^)]+)', unicode(el))
+    return parser.parse(match.group(1)).strftime('%Y-%m-%d')
+
+
+def rent_stabilized(soup):
+    el = soup.find(text=re.compile('Housing-Rent Stabilization'))
+    match = re.search(r'Housing-Rent Stabilization\s+(\d+)', unicode(el))
+    if not match:
+        return 0
+    else:
+        return int(match.group(1))
+
+
+def extract(data):
+    """
+    Extract Quarterly Statement of Account data from data
+    """
+    soup = BeautifulSoup(data)
+    soup.find(text=re.compile('Housing-Rent Stabilization'))
+
+    stabilized_units = rent_stabilized(soup)
+    data = {
+        "rentStabilized": stabilized_units > 0,
+        "units": stabilized_units,
+        "activityThrough": activity_through(soup),
+        "ownerName": owner_name(soup),
+        # "propertyAddress": property_address(soup), ## TODO
+        # "mailingAddress": mailing_address(soup), ## TODO
+        # "abatements": abatements(soup), ## TODO
+        # "billableAssessedValue": billable_assessed_value(soup), ## TODO
+        # "taxRate": tax_rate(soup) ## TODO
+    }
+
+    return data
+
+
+def main(paths):
+    """
+    Process a list of filenames with Quarterly Statement of Account data.
+    """
+    writer = csv.DictWriter(sys.stdout, HEADERS)
+    writer.writeheader()
+    for path in paths:
+        bbl = path.split(os.path.sep)[-2]
+        # date = path.split(os.path.sep)[-1].split(' - ')
+        with open(path, 'r') as f:
+            data = extract(f.read())
+        data.update({
+            # 'date': date,
+            'bbl': bbl.replace('-', '')
+        })
+        writer.writerow(data)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
