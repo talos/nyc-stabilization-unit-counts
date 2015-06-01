422f1d64f8d03b66a21ebb9dbc74939525c643af
john krauss <irving.krauss@gmail.com>
2015-06-01 00:11:48 +0000

    temp parse file, then mv

--- parse.sh
+++ parse.sh
@@ -3,10 +3,13 @@
 source .env/bin/activate
 
 pushd quarterly-property-tax-bills-pdf
-nodejs run.js ../data/ > ../data/pdf-tax-bills.csv 2>../data/pdf-tax-bills.log &
+nodejs run.js ../data/ > ../data/pdf-tax-bills.tmp.csv 2>../data/pdf-tax-bills.log &
 popd
 
 pushd quarterly-statement-of-account-html
-python scrape.py ../data/ > ../data/html-statement-of-account.csv 2>../data/html-statement-of-account.log &
+python scrape.py ../data/ > ../data/html-statement-of-account.tmp.csv 2>../data/html-statement-of-account.log &
 popd
 
+wait
+mv data/html-statement-of-account.tmp.csv data/html-statement-of-account.csv
+mv data/pdf-tax-bills.tmp.csv data/pdf-tax-bills.csv
