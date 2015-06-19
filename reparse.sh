0faaf0677dcc2d1dee79eca7050097fa14a3fdd6
john krauss <irving.krauss@gmail.com>
2015-06-19 12:27:48 +0000

    work with single rawdata file

--- reparse.sh
+++ reparse.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 source .env/bin/activate
-time python parse.py data/ >data/pdf-tax-bills.csv 2>data/pdf-tax-bills.log &
-time python quarterly-statement-of-account-html/scrape.py data/ >data/html-statement-of-account.csv 2>data/html-statement-of-account.log  &
+time python parse.py data/ >data/rawdata.csv 2>data/rawdata.log &
