d9869b5d7170398b25a0caad4fcb8f9b183eb5cd
john krauss <irving.krauss@gmail.com>
2015-07-17 19:00:16 +0000

    scratch work

--- scratch.sql
+++ scratch.sql
@@ -1,8 +1,50 @@
+/* comparisons of RGB data to our totals. */
+
+/*CREATE VIEW borough_year_summary AS
+SELECT ucbbl/1000000000 borough, year, SUM(unitcount) as sum
+FROM joined_nocrosstab
+GROUP BY ucbbl/1000000000, year
+ORDER BY ucbbl/1000000000, year;*/
+
+SELECT
+dof.borough, rgb.year,
+dof.net as dof_net, rgb.net as rgb_net,
+    ((rgb.net - dof.net) * 100.0 / rgb.net)::int net_err,
+dof.sub as dof_sub, rgb.total_sub as rgb_sub,
+    ((rgb.total_sub - dof.sub) * 100.0 / rgb.total_sub)::int sub_err,
+dof.add as dof_add, rgb.total_add as rgb_add,
+    ((rgb.total_add - dof.add) * 100.0 / rgb.total_add)::int add_err
+FROM
+(SELECT a.ucbbl/1000000000 borough, b.year,
+       SUM(b.unitcount - a.unitcount) net,
+       SUM(CASE WHEN b.unitcount - a.unitcount < 0 THEN b.unitcount - a.unitcount ELSE 0 END) AS sub,
+       SUM(CASE WHEN b.unitcount - a.unitcount > 0 THEN b.unitcount - a.unitcount ELSE 0 END) AS "add"
+FROM joined_nocrosstab a, joined_nocrosstab b
+WHERE a.ucbbl = b.ucbbl
+      AND a.year = b.year - interval '1 year'
+GROUP BY borough, b.year
+ORDER BY borough, b.year) dof, rgb
+WHERE rgb.borough = dof.borough AND rgb.year = date_part('year', dof.year)
+ORDER BY rgb.borough, rgb.year;
+
+
 SELECT bbl, unitstotal, unitsres FROM
 nyc_pluto pl JOIN registrations reg
 ON pl.bbl = reg.dhcrbbl
 WHERE ucbbl IS NULL
 ORDER BY ucbbl;
+
+select ucbbl,
+"2007uc", "2007est",
+"2008uc", "2008est",
+"2009uc", "2009est",
+"2010uc", "2010est",
+"2011uc", "2011est",
+"2012uc", "2012est",
+"2013uc", "2013est",
+"2014uc", "2014est"
+from joined
+order by ucbbl;
 
 SELECT dhcrbbl / 1000000000 as borough, COUNT(distinct dhcrbbl) FROM
 registrations reg
@@ -21,6 +63,20 @@ sum("2008uc") "2008", sum("2009uc") "2009", sum("2010uc") "2010",
 sum("2011uc") "2011", sum("2012uc") "2012", sum("2013uc") "2013",
 sum("2014uc") "2014"
 from joined
+group by borough;
+
+copy (select borough,
+sum("2008uc") - sum("2007uc") "2008",
+sum("2009uc") - sum("2008uc") "2009",
+sum("2010uc") - sum("2009uc") "2010",
+sum("2011uc") - sum("2010uc") "2011",
+sum("2012uc") - sum("2011uc") "2012",
+sum("2013uc") - sum("2012uc") "2013",
+sum("2014uc") - sum("2013uc") "2014"
+from joined
+group by borough)
+to stdout WITH CSV DELIMITER ',' HEADER;
+
 where
 not (
   "2007uc" >= coalesce("2008uc", 0) - 2 AND
