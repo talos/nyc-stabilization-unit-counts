2264aa69b74feac99b0d47ae294cf132e7346954
john krauss <irving.krauss@gmail.com>
2015-08-31 19:57:36 +0000

    adding basic graphing

--- site/js/taxbills.js
+++ site/js/taxbills.js
@@ -1,5 +1,6 @@
 /*jshint browser:true, camelcase: false*/
-/*globals $: false, Handlebars: false, querystring: false, google: false*/
+/*globals $: false, Handlebars: false, querystring: false, google: false,
+d3: false, nv: false*/
 
 (function () {
 
@@ -84,6 +85,58 @@
    */
   function showGraphs(data) {
     $('#history').empty().append(graphTemplate(data));
+
+    var rentStabilization = data.table.where(function (t) {
+      return t.key === 'Housing-Rent Stabilization';
+    }).groupBy(function (t) {
+      return t.dueDate;
+    }).orderBy(function (t) {
+      return new Date(t[0].dueDate);
+    }).select(function (t) {
+      var sumapts = 0,
+          regnos = {};
+      for (var i = 0; i < t.length; i += 1) {
+        if (!(t[i].meta in regnos)) {
+          sumapts += Number(t[i].apts);
+          regnos[t[i].meta] = true;
+        }
+      }
+
+      return {x: new Date(t[0].dueDate).getFullYear(), y: sumapts};
+    });
+
+    var chart = nv.models.lineChart()
+                  .forceY([0])
+                  .forceX([2007, 2015])
+    //  .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+    //  .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+    //  .transitionDuration(350)  //how fast do you want the lines to transition?
+    //  .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+    //  .showYAxis(true)        //Show the y-axis
+    //  .showXAxis(true)        //Show the x-axis
+    ;
+
+    chart.xAxis     //Chart x-axis settings
+    .axisLabel('Registration Year');
+    //.tickFormat(d3.format(',r'));
+
+    chart.yAxis     //Chart y-axis settings
+    .axisLabel('Rent Stabilized Units');
+    //.tickFormat(d3.format('.02f'));
+
+    /* Done setting the chart up? Time to render it!*/
+
+    d3.select('#graphs svg')    //Select the <svg> element you want to render the chart in.   
+    .datum([{
+      values: rentStabilization,
+      key: 'Rent Stabilized Units',
+      color: '#000'
+    }])         //Populate the <svg> element with chart data...
+    .call(chart);          //Finally, render the chart!
+
+    //Update the chart when window resizes.
+    nv.utils.windowResize(function() { chart.update(); });
+    //return chart;
   }
 
   /**
