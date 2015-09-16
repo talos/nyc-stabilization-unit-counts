2d6e85298a9b51575bbca5018ca072aee1081670
john krauss <irving.krauss@gmail.com>
2015-09-16 11:47:57 +0000

    first stab at s3 backup for pdfs

---
+++ upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Upload PDFs that have been converted to text to S3, then delete
+
+find data/ -iname *.pdf | head -n 100 | while read pdf; do
+  #echo "$pdf"
+  filename=$(basename "$pdf" .pdf)
+  path=$(dirname "$pdf")
+  txtpath="$path/$filename.txt"
+
+  if [ -e "$txtpath" ]; then
+    aws s3 cp "$pdf" "s3://taxbills.nyc.test/$pdf"
+  fi
+done
