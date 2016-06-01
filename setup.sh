8bbb83cb81c85ffd1f5b40aabfdae3f37470d98b
Ralph Yozzo <ralph@brooklynmarathon.com>
2016-06-01 14:04:58 -0400

    add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!
+sudo apt-get install python-dev python-pip python-virtualenv \
+                         build-essential libxml2-dev libxslt1-dev xpdf
+virtualenv .env
+source .env/bin/activate
+pip install -r requirements.txt
+
+# python download.py <house no> '<street name with suffix>' <borough number>
+python download.py : 49 'W. 119TH ST' 1
+
+python parse.py testdata  testoutput.csv
