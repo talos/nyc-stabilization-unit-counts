48cd929e8da0f926cab3ebf15ef498d903b77eb1
Ralph Yozzo <ralph@brooklynmarathon.com>
2016-06-03 10:25:58 -0400

    add outstanding charges

--- searchfor.sh
+++ searchfor.sh
@@ -1,6 +1,6 @@
 #!
 find data/ -name "*.pdf" -exec pdftotext -layout {} \; -print 
-find data/ -name "*.txt" -exec grep -H "\\$" {} \; -print |grep -i unpaid|grep -v "\$0.00" >unpaid_trainingdata.txt
+find data/ -name "*.txt" -exec grep -H "\\$" {} \; -print |egrep -i 'unpaid|outstanding charges'|grep -v "\$0.00" >unpaid_trainingdata.txt
 find data/ -name "*.txt" -exec grep -Hi "property address" {} \;  >propertyaddress_trainingdata.txt
 
 # sed -r "s/(.*)[$](.*)/\2 , http://taxbills.nyc/\1/" unpaid_trainingdata.txt |sed "s/:/,/" | sort -rn | tee unpaid_sorted.csv
