fc76c861244eb93a25bfe2c44eb7aef1eaf53006
Ralph Yozzo <ralph@brooklynmarathon.com>
2016-06-02 21:17:30 -0400

    Add training data

--- searchfor.sh
+++ searchfor.sh
@@ -1,3 +1,6 @@
 #!
-find data/ -name "*.pdf" -exec pdftotext {} \; -print 
-find data/ -name "*.txt" -exec grep -H "\\$" {} \; -print |grep -i unpaid|grep -v "\$0.00"
+find data/ -name "*.pdf" -exec pdftotext -layout {} \; -print 
+find data/ -name "*.txt" -exec grep -H "\\$" {} \; -print |grep -i unpaid|grep -v "\$0.00" >unpaid_trainingdata.txt
+find data/ -name "*.txt" -exec grep -Hi "property address" {} \;  >propertyaddress_trainingdata.txt
+
+# ./createcsv.py *_traindata.txt
