14cd2b8ecf727946b2348546f17773c6f20bb227
Ralph Yozzo <fedex1@gmail.com>
2016-06-02 21:43:15 -0400

    add big unpaid items

--- searchfor.sh
+++ searchfor.sh
@@ -4,6 +4,6 @@ find data/ -name "*.txt" -exec grep -H "\\$" {} \; -print |grep -i unpaid|grep -
 find data/ -name "*.txt" -exec grep -Hi "property address" {} \;  >propertyaddress_trainingdata.txt
 
 # sed -r "s/(.*)[$](.*)/\2 , http://taxbills.nyc/\1/" unpaid_trainingdata.txt |sed "s/:/,/" | sort -rn | tee unpaid_sorted.csv
-sed "s/:/,/" unpaid_trainingdata.txt | sed -r "s?(.*)[$](.*)?\2 , http://taxbills.nyc/\1?" | sort -rn | tee unpaid_sorted.csv
+sed "s/:/\t/" unpaid_trainingdata.txt | sed -r "s?(.*)[$](.*)?\2 , http://taxbills.nyc/\1?" | sort -rn | tee unpaid_sorted.tsv
 
 # ./createcsv.py *_traindata.txt
