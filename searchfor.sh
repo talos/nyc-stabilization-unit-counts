0755c12e3ad97aaa2c524ece713743c1f13bad8a
Ralph Yozzo <fedex1@gmail.com>
2016-06-09 22:47:20 -0400

    add uniq

--- searchfor.sh
+++ searchfor.sh
@@ -10,6 +10,7 @@ grep 2016 unpaid_sorted.tsv  |sed "s?\(/[0-9][0-9][0-9][0-9]/\).*?\1?" |sort -u 
 # sed 's?(/[0-9][0-9][0-9][0-9]/).*?\1?' unpaid_sorted_with_link.csv |tee x.csv
 sed "s?\(.*\),.*\([0-9]/.*/[0-9][0-9][0-9][0-9]/\).*?\1,_SSS_:\2 _XXX_:\2 _YYY_:\2?" unpaid_sorted_with_link.csv  |sed  "s?/??g" | sed "s?_SSS_:?http://nycprop.nyc.gov/nycproperty/StatementSearch\?stmtDate=20160603\&stmtType=SOA\&bbl=?" | sed "s?_XXX_:?,https://city.tidalforce.org/#\!/item/?" |sed "s?_YYY_:?,https://taxhistory.brooklyncoop.org/view1/?" | tee unpaid_report.csv
 
+# add address sed "s?\(.*\),.*\([0-9]/.*/[0-9][0-9][0-9][0-9]/\).*?grep '\2' *prop*train*.txt?" unpaid_sorted_with_link.csv |bash |grep 2016
 # add https://city.tidalforce.org/#!/item/1017180119 and https://taxhistory.brooklyncoop.org/view1/1017180119
 # http://nycprop.nyc.gov/nycproperty/StatementSearch?bbl=1013770064&stmtDate=20160603&stmtType=SOA
 
