ff99186793ca1b0e2e3cb3c33e88ac424f5db06e
john krauss <irving.krauss@gmail.com>
2016-09-23 00:38:40 +0000

    info on loading in the dhcr data

--- import.sh
+++ import.sh
@@ -6,4 +6,9 @@ export PGHOST=localhost
 export PGPORT=54321
 export PGDATABASE=postgres
 
+# git clone https://github.com/clhenrick/dhcr-rent-stabilized-data.git
+pushd ../dhcr-rent-stabilized-data
+pgloader pgloader.load
+popd
+
 psql -f cross-tab-rs-counts.sql
