ab6df248702fe5cb44d02573c448ef25c20d182c
john krauss <irving.krauss@gmail.com>
2016-10-15 10:46:53 +0000

    remove requests entirely, just use subprocess and wget

--- download_direct.py
+++ download_direct.py
@@ -8,8 +8,8 @@ import logging
 import os
 import sys
 import time
-import requests
 import subprocess
+from subprocess import CalledProcessError
 from dateutil import parser
 from download import save_file_from_stream, SESSION, find_extension
 
@@ -91,7 +91,7 @@ def main(period, borough, block, lot, *_):
 
     filenames = os.listdir(bbldir)
     nostatement_fname = 'nostatement.' + period + '.txt'
-    if docname + '.pdf' in filenames:
+    if (docname + '.pdf' in filenames) or (docname + '.txt' in filenames):
         LOGGER.info(u'Already downloaded "%s" for BBL %s, skipping',
                     docname, bbl)
         return
@@ -107,42 +107,23 @@ def main(period, borough, block, lot, *_):
 
     url = 'http://nycprop.nyc.gov/nycproperty/StatementSearch?' + \
             'bbl={bbl}&stmtDate={period}&stmtType=SOA'.format(period=period, bbl=bbl)
-    resp = requests.get(url)
-    extension = find_extension(resp)
-
-    if resp.url == u'http://nycprop.nyc.gov/nycproperty/nynav/jsp/StatementNotFound.jsp':
-        LOGGER.warn('No statement for %s', url)
-        subprocess.check_call('touch "{}/{}" &'.format(bbldir, nostatement_fname), shell=True)
-        return
-
-    if extension != 'pdf':
-        LOGGER.warn('Cannot download %s, has wrong extension: %s', url, extension)
-        raise requests.exceptions.ConnectionError()
 
     filename = os.path.join(bbldir, docname)
     LOGGER.info('Saving %s for %s', filename, bbl)
-    #save_file_from_stream(resp, filename)
-    subprocess.check_call('wget -O "{filename}.pdf" "{url}" &'.format(
-        filename=filename,
-        url=url
-    ), shell=True)
-    time.sleep(2)
+    subprocess.check_call('wget --max-redirect=0 -O "{filename}.pdf" "{url}" '
+                          ' || (rm "{filename}".pdf && touch "{nofilemarker}")'.format(
+                              filename=filename,
+                              url=url,
+                              nofilemarker=os.path.join(bbldir, nostatement_fname)
+                          ), shell=True)
+    #time.sleep(1)
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         with open(sys.argv[2]) as infile:
             for line in infile:
-                wait = 10
-                while True:
-                    bbl_ = line.strip().split('\t')
-                    try:
-                        main(sys.argv[1], *bbl_)
-                        break
-                    except requests.exceptions.ConnectionError as err:
-                        LOGGER.warn(u'ConnectionError on %s: %s, waiting %s seconds',
-                                    bbl_, err, wait)
-                        time.sleep(wait)
-                        wait *= 2
+                bbl_ = line.strip().split('\t')
+                main(sys.argv[1], *bbl_)
     else:
         sys.stderr.write(u'''
 Usage:
