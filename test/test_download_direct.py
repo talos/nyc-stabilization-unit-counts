f9ce818fa68f85db774e0dce8dcb6b95f456bd77
talos <jkrauss@heapanalytics.com>
2018-06-30 08:07:07 -0400

    actually test whether file was downloaded

--- test/test_download_direct.py
+++ test/test_download_direct.py
@@ -1,7 +1,9 @@
+import os
 from download_direct import main
 
 def test_download_one_bill():
     '''
     Confirm we can download a bill without throwing an error.
     '''
-    main('20150605', 'SOA', '3', '1772', '74')
+    assert main('20170602', 'SOA', '3', '1772', '74') == 0
+    assert os.path.exists('./data/3/01772/0074/June 2, 2017 - Quarterly Property Tax Bill.pdf')
