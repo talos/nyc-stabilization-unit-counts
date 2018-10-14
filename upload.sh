4c11c4633f6d89784d487fae5eb9ed1314fa7385
john krauss <irving.krauss@gmail.com>
2018-10-14 17:47:32 +0000

    also upload txt and html

--- upload.sh
+++ upload.sh
@@ -12,3 +12,19 @@ find data/ -iname *.pdf | while read pdf; do
     aws s3 cp "$pdf" "s3://taxbills.nyc/$pdf" --acl public-read && rm "$pdf"
   fi
 done
+
+find data/ -iname *.txt | while read txt; do
+  filename=$(basename "$txt" .txt)
+  path=$(dirname "$txt")
+
+  echo "copy $txt"
+  aws s3 cp "$txt" "s3://taxbills.nyc/$txt" --acl public-read
+done
+
+find data/ -iname *.html | while read html; do
+  filename=$(basename "$html" .html)
+  path=$(dirname "$html")
+
+  echo "copy $html"
+  aws s3 cp "$html" "s3://taxbills.nyc/$html" --acl public-read
+done
